67ed452f6a70a6e83297bc2e01d8d218d94ca9f0
rodrigflorencia <rodrig.florencia@gmail.com>
2026-06-19 18:13:35 -0300

    Update Poligono.h

--- Poligono.h
+++ Poligono.h
@@ -8,16 +8,14 @@ private:
     Punto *vertices;
     int cantidad;
     double productoCruz(const Punto &A, const Punto &B, const Punto &C) const;
-    bool esConvexo() const;
-    bool sentidoAntihorario() const;
-    void liberarMemoria(double **C, int n) const;
-
+   
 public:
     Poligono(int n);
     ~Poligono();
     void agregarPunto(const Punto &p);
-
-    double costoTriangulacion() const;
+    bool esConvexo() const;
+    bool sentidoAntihorario() const;
     int getTotalVertices() const;
+    const Punto & getPunt(int i) const;
 };
 #endif // POLIGONO_H
