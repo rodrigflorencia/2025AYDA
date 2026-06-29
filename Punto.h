1c21eb87570619756e7f06cd1a7788503a323e89
rodrigflorencia <rodrig.florencia@gmail.com>
2026-06-29 14:28:13 -0300

    Add const qualifier to productoCruz method

--- Punto.h
+++ Punto.h
@@ -17,7 +17,7 @@ public:
     double getY() const;
     void setX(double x);
     void setY(double y);
-    double productoCruz(const Punto &B, const Punto &C);
+    double productoCruz(const Punto &B, const Punto &C) const;
 
 };
 #endif // PUNTO_H
